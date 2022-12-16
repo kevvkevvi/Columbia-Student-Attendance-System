f47875120b9a3de786ed0b86ac9b0373cd62ffe0
BruceGeng <pkugeng@pku.edu.cn>
2022-12-15 21:52:36 -0500

    add lots of GET methods in attendance

--- microservice/attendance/DBResource.py
+++ microservice/attendance/DBResource.py
@@ -24,11 +24,62 @@ class DBResource:
         cur = conn.cursor()
         return conn, cur
 
-    def get_by_key(self, key):
-
-        sql = "SELECT * FROM f22_databases.columbia_students where guid=%s";
-        res = self.cur.execute(sql, args=key)
-        result = self.cur.fetchone()
-
+    def get_sections(self):
+        sql = "SELECT * FROM sections"
+        res = self.cur.execute(sql)
+        result = self.cur.fetchall()
         return result
 
+    def get_classes(self):
+        sql = "SELECT * FROM class"
+        res = self.cur.execute(sql)
+        result = self.cur.fetchone()
+        return result
+
+    def get_students(self):
+        sql = "SELECT * FROM students"
+        res = self.cur.execute(sql)
+        result = self.cur.fetchone()
+        return result
+
+    def get_section(self, key):
+        sql = "SELECT * FROM sections where call_no=%s"
+        res = self.cur.execute(sql, args=key)
+        result = self.cur.fetchone()
+        return result
+
+    def get_section_attendances(self, key):
+        sql = "SELECT * FROM class where call_no=%s"
+        res = self.cur.execute(sql, args=key)
+        result = self.cur.fetchall()
+        return result
+    
+    def get_section_attendance(self, call_no, date):
+        sql = "SELECT * FROM class where call_no=%s and date=%s"
+        res = self.cur.execute(sql, args=(call_no, date))
+        result = self.cur.fetchone()
+        return result
+
+    def get_section_class_students(self, call_no, date):
+        sql = "SELECT * FROM students where call_no=%s and date=%s"
+        res = self.cur.execute(sql, args=(call_no, date))
+        result = self.cur.fetchone()
+        return result
+
+    def get_section_class_student(self, call_no, date, uni):
+        sql = "SELECT * FROM students where call_no=%s and date=%s and UNI=%s"
+        res = self.cur.execute(sql, args=(call_no, date, uni))
+        result = self.cur.fetchone()
+        return result
+    
+    def get_section_students(self, call_no):
+        sql = "SELECT call_no, UNI, date FROM students NATURAL JOIN sections where call_no=%s"
+        res = self.cur.execute(sql, args=(call_no))
+        result = self.cur.fetchone()
+        return result
+
+    def get_section_student(self, call_no, uni):
+        sql = "SELECT call_no, UNI, date FROM students NATURAL JOIN sections where call_no=%s and UNI=%s"
+        res = self.cur.execute(sql, args=(call_no, uni))
+        result = self.cur.fetchone()
+        return result
--- microservice/attendance/application.py
+++ microservice/attendance/application.py
@@ -1,5 +1,5 @@
 from flask import Flask, Response, request
-from datetime import datetime
+import datetime
 import json
 from DBResource import *
 from flask_cors import CORS
@@ -13,33 +13,113 @@ app = Flask(__name__,
 CORS(app)
 
 
-@app.get("/api/health")
-def get_health():
-    t = str(datetime.now())
-    msg = {
-        "name": "F22-Starter-Microservice",
-        "health": "Good",
-        "at time": t
-    }
-
-    # DFF TODO Explain status codes, content type, ... ...
-    result = Response(json.dumps(msg), status=200, content_type="application/json")
-
-    return result
+class DateEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.date):
+            return obj.strftime("%Y-%m-%d")
+        else:
+            return json.JSONEncoder.default(self, obj)
 
 
-@app.route("/api/students/<uni>", methods=["GET"])
-def get_student_by_uni(uni):
-
-    result = ColumbiaStudentResource.get_by_key(uni)
-
+@app.route("/api/sections", methods=["GET"])
+def get_all_sections():  # get all sections
+    result = DBResource().get_sections()
     if result:
         rsp = Response(json.dumps(result), status=200, content_type="application.json")
     else:
         rsp = Response("NOT FOUND", status=404, content_type="text/plain")
-
     return rsp
+
+
+@app.route("/api/class", methods=["GET"])
+def get_all_classes():  # get all classes
+    result = DBResource().get_classes()
+    if result:
+        rsp = Response(json.dumps(result), status=200, content_type="application.json")
+    else:
+        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+    return rsp
+
+
+@app.route("/api/students", methods=["GET"])
+def get_all_students():  # get all students
+    result = DBResource().get_students()
+    if result:
+        rsp = Response(json.dumps(result), status=200, content_type="application.json")
+    else:
+        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+    return rsp
+
+
+@app.route("/api/sections/<call_no>", methods=["GET"])
+def get_section_by_call_no(call_no):  # get section <call_no>'s course_name and enrollment
+    result = DBResource().get_section(call_no)
+    if result:
+        rsp = Response(json.dumps(result), status=200, content_type="application.json")
+    else:
+        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+    return rsp
+
+
+@app.route("/api/sections/<call_no>/class/", methods=["GET"])
+def get_section_attendances(call_no):  # get section <call_no>'s classes' date and attendance
+    result = DBResource().get_section_attendances(call_no)
+    if result:
+        rsp = Response(json.dumps(result, cls=DateEncoder), status=200, content_type="application.json")
+    else:
+        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+    return rsp
+
+
+@app.route("/api/sections/<call_no>/class/<date>", methods=["GET"])
+def get_section_attendance(call_no, date):  # get <call_no>'s attendance on <date>
+    result = DBResource().get_section_attendance(call_no, date)
+    if result:
+        rsp = Response(json.dumps(result, cls=DateEncoder), status=200, content_type="application.json")
+    else:
+        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+    return rsp
+
+
+@app.route("/api/sections/<call_no>/class/<date>/students/", methods=["GET"])
+def get_section_class_students(call_no, date):  # get all students' attendance records on <call_no><date>
+    result = DBResource().get_section_class_students(call_no, date)
+    if result:
+        rsp = Response(json.dumps(result, cls=DateEncoder), status=200, content_type="application.json")
+    else:
+        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+    return rsp
+
+
+@app.route("/api/sections/<call_no>/class/<date>/students/<uni>", methods=["GET"]) 
+def get_section_class_student(call_no, date, uni):  # get <uni>'s attendance record on <call_no><date>
+    result = DBResource().get_section_class_student(call_no, date, uni)
+    if result:
+        rsp = Response(json.dumps(result, cls=DateEncoder), status=200, content_type="application.json")
+    else:
+        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+    return rsp
+
+
+@app.route("/api/sections/<call_no>/students/", methods=["GET"])
+def get_section_students(call_no, uni):  # get all students attendance records of <call_no>
+    result = DBResource().get_section_students(call_no)
+    if result:
+        rsp = Response(json.dumps(result, cls=DateEncoder), status=200, content_type="application.json")
+    else:
+        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+    return rsp
+
+
+@app.route("/api/sections/<call_no>/students/<uni>", methods=["GET"])
+def get_section_student(call_no, uni):  # get <uni> attendance records of <call_no>
+    result = DBResource().get_section_student(call_no, uni)
+    if result:
+        rsp = Response(json.dumps(result, cls=DateEncoder), status=200, content_type="application.json")
+    else:
+        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+    return rsp
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5011)
-
