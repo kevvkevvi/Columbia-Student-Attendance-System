3603425931d8e46b224e3da3eb19f19fefd8758a
Kevin Li <kjl2185@columbia.edu>
2022-12-22 13:35:41 -0500

    shows courses correctly, need to implement click course --> Attendance.jsx component that shows specific course attendance details for student with <uni>

--- ui/student-attendance-system/src/components/Course.jsx
+++ ui/student-attendance-system/src/components/Course.jsx
@@ -1,38 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const Course = ({ uni, call_no }) => {
-    return (
-        <div>
-            <h1>Course Name</h1>
-            <p>Course Description</p>
-            <button onClick={() => checkIn(uni, call_no)}>Check In</button>
-        </div>
-    );
-}
+const Course = ({ uni }) => {
+    const [courses, setCourses] = useState(null);
 
-const checkIn = (uni, call_no) => {
-    // Make POST request to /api/attendance endpoint
-    fetch('http://ec2-34-224-37-72.compute-1.amazonaws.com:5011/api/enrollments', {
-        method: 'POST',
-        body: JSON.stringify({ call_no, uni }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            // Handle success or failure of request
-        })
-        .catch(error => {
-            console.error(error);
-        });
-}
-
-const Courses = ({ uni }) => {
-    const [courses, setCourses] = useState([]);
-    // console.log("we're here")
     useEffect(() => {
         const fetchCourses = async () => {
             try {
@@ -42,88 +13,88 @@ const Courses = ({ uni }) => {
                 console.error(error);
             }
         };
+
         fetchCourses();
     }, [uni]);
 
+    if (!courses) {
+        return <div>Loading courses...</div>;
+    }
+
+    console.log(courses);
+
     return (
-        // console.log(courses);
-        <div className="courses">
+        <div>
+            <h1>Courses</h1>
             {courses.map((course) => (
-                <Course key={course.course_number} courseName={course.course_name} courseNumber={course.course_number} />
+                <div className="courses" key={course.call_no}>
+                    <p>Call Number: {course.call_no}</p>
+                </div>
             ))}
         </div>
     );
 };
 
-export default Courses;
+export default Course;
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-// import React, { useState, useEffect } from "react";
+// import React, { useEffect, useState } from "react";
 // import axios from "axios";
-// import '../App.css';
 
-// const baseUrl = "http://ec2-34-224-37-72.compute-1.amazonaws.com:5011";
-
-// function Course(props) {
-//     const { uni } = props;
-//     const [courses, setCourses] = useState([]);
-
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             try {
-//                 // Make a request to the student microservice to get the student's name
-//                 const studentResponse = await axios(
-//                     `http://6156projstudentmicroservice-env.eba-ds6ar3x2.us-east-2.elasticbeanstalk.com/student/${uni}`
-//                 );
-//                 const studentName = studentResponse.data.name;
-
-//                 // Use the student's name to get the courses they are taking
-//                 const result = await axios(
-//                     `${baseUrl}/section/name/${studentName}`
-//                 );
-//                 setCourses(result.data);
-//             } catch (error) {
-//                 console.error(error);
-//             }
-//         };
-//         fetchData();
-//     }, [uni]);
-
-//     if (!courses) {
-//         return <p>Loading courses data...</p>;
-//     }
-
+// const Course = ({ uni, call_no }) => {
 //     return (
-//         <div className="courses">
-//             {courses.map((course) => (
-//                 <div className="course" key={course.call_no}>
-//                     <h1 className="course h1">{course.course_name}</h1>
-//                     <p className="course p">{course.enrollment_number}</p>
-//                     <button className="course button" onClick={() => checkIn(course.call_no, course.enrollment_number)}>Check In</button>
-//                 </div>
-//             ))}
+//         <div>
+//             <h1>Course Name</h1>
+//             <p>Course Description</p>
+//             <button onClick={() => checkIn(uni, call_no)}>Check In</button>
 //         </div>
 //     );
 // }
 
-// function checkIn(callNo, enrollmentNumber) {
-//     axios.post(`http://ec2-44-204-239-194.compute-1.amazonaws.com:5011/api/sections/edit_enrollment_number`, {
-//         call_no: callNo,
-//         enrollment_number: enrollmentNumber + 1,
-//     });
+// const checkIn = (uni, call_no) => {
+//     // Make POST request to /api/attendance endpoint
+//     fetch('http://ec2-34-224-37-72.compute-1.amazonaws.com:5011/api/enrollments', {
+//         method: 'POST',
+//         body: JSON.stringify({ call_no, uni }),
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//         .then(res => res.json())
+//         .then(data => {
+//             console.log(data);
+//             // Handle success or failure of request
+//         })
+//         .catch(error => {
+//             console.error(error);
+//         });
 // }
 
+// const Courses = ({ uni }) => {
+//     const [courses, setCourses] = useState([]);
+//     // console.log("we're here")
+//     useEffect(() => {
+//         const fetchCourses = async () => {
+//             try {
+//                 const response = await axios.get(`http://ec2-34-224-37-72.compute-1.amazonaws.com:5011/enrollments/uni/${uni}`);
+//                 setCourses(response.data);
+//             } catch (error) {
+//                 console.error(error);
+//             }
+//         };
+//         fetchCourses();
+//     }, [uni]);
 
-// export default Course;
+//     return (
+//         // console.log(courses);
+//         <div className="courses">
+//             {courses.map((course) => (
+//                 <Course key={course.call_no} uni={course.uni} courseNumber={course.course_number} />
+//             ))}
+//         </div>
+//     );
+// };
+
+// export default Courses;
